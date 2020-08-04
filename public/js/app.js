fc5fb61a64f448b77ffd412351e50dd18b2d01da
wintesfamariam <wintesfamariam@gmail.com>
2020-08-04 07:39:30 -0500

    heroku setup

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const location = search.value;
 
-        fetch('http://localhost:3000/weather?address='+location).then((response) => {
+        fetch('/weather?address='+location).then((response) => {
             response.json().then((data) => {
                 if (data.error) {
                    // console.log(data.error);
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-})
+});
 
 
 
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000 
 
 //paths for express config
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -103,8 +104,8 @@ app.get("*",(req,res)=>{
     })
 });
 
-app.listen(3000,()=>{
-    console.log("server up on port 3000");
+app.listen(port,()=>{
+    console.log("server up on port "+port);
 });
 
 
